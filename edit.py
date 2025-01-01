3fc7fd8cfe63ee33cc1a3404c118f9271e06be5e
warren <warren@localhost.localdomain>
2025-01-01 20:16:03 +0800

    Added _posts/2024-12-30-ssh_play_video.md

---
+++ edit.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+# Read file name
+file = sys.argv[1]
+
+file_list = os.listdir("_posts")
+
+# Open or creat file
+os.system(f"nvim {file}")
+
+os.system("git add .")
+
+# Check and add comments
+if file.split("/")[-1] in file_list:
+    os.system(f"git commit -m 'Updated {file}'")
+else:
+    os.system(f"git commit -m 'Added {file}'")
+
+# Push
+os.system("proxychains4 git push")
